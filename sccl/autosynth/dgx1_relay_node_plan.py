c417aa3492f038a2fd86ee0aacb4c8c821bd6831
Olli Saarikivi <olsaarik@microsoft.com>
2021-07-13 12:19:37 -0700

    Autosynth finish top level logic (untested)

--- sccl/autosynth/dgx1_relay_node_plan.py
+++ sccl/autosynth/dgx1_relay_node_plan.py
@@ -12,13 +12,12 @@ class DGX1RelayNodePlan:
     def __init__(self, local_topo):
         self.local_topo = local_topo
     
-    def synthesize(self, world_size, collective_names, logging=False):
+    def synthesize(self, world_size, collective_name, logging=False):
         if world_size % self.local_topo.num_nodes() != 0:
             raise RuntimeError('Local machine size does not evenly divide world size.')
         num_machines = world_size // self.local_topo.num_nodes()
-        for name in collective_names:
-            if name == 'Alltoall':
-                yield self._synthesize_alltoall(num_machines, logging)
+        if collective_name == 'Alltoall':
+            return self._synthesize_alltoall(num_machines, logging)
     
     def _synthesize_alltoall(self, num_machines, logging):
         outbound, inbound = self._select_root_nodes()
@@ -26,7 +25,7 @@ class DGX1RelayNodePlan:
         scatter_coll = scatter(8, inbound)
         gather_algo = solve_least_steps(dgx1(), gather_coll, logging=logging)
         scatter_algo = solve_least_steps(dgx1(), scatter_coll, logging=logging)
-        synthesize_gather_scatter_distributed_alltoall(num_machines, gather_algo, scatter_algo, logging)
+        return synthesize_gather_scatter_distributed_alltoall(num_machines, gather_algo, scatter_algo, logging)
 
     def _select_root_nodes(self):
         # TODO: is this always the right thing?
