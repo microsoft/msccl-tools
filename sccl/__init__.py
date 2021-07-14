8e5d608e9b8ae47e8a2a61750bfc9ee4e20c46ae
Olli Saarikivi <olsaarik@microsoft.com>
2021-07-14 16:21:29 -0700

    Add torch_distributed_launch parm to autosynth

--- sccl/__init__.py
+++ sccl/__init__.py
@@ -3,5 +3,5 @@
 
 import sccl.autosynth as _autosynth
 
-def autosynth(logging=False):
-    _autosynth.init(logging=logging)
+def autosynth(logging=False, torch_distributed_launch=False):
+    _autosynth.init(logging=logging, torch_distributed_launch_hack=torch_distributed_launch)
