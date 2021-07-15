02e28b30e905af57af12ea7b9b7371ecae41fc7a
Olli Saarikivi <olsaarik@microsoft.com>
2021-07-15 13:19:49 -0700

    Improve autosynth API
    autosynth() is now called init() and takes a num_subprocesses parameter.

--- sccl/__init__.py
+++ sccl/__init__.py
@@ -1,7 +1,4 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 
-import sccl.autosynth as _autosynth
-
-def autosynth(logging=False, torch_distributed_launch=False):
-    _autosynth.init(logging=logging, torch_distributed_launch_hack=torch_distributed_launch)
+from sccl.autosynth import init
