8a84a9f4273d3c9915e14b1274372c5e579f6c90
Olli Saarikivi <olsaarik@microsoft.com>
2021-07-16 15:45:47 -0700

    New approach to handling both MPI and launchers

--- sccl/autosynth/__init__.py
+++ sccl/autosynth/__init__.py
@@ -4,49 +4,88 @@
 from sccl.topologies.nvidia import nvlink_only
 from sccl.autosynth.dgx1_relay_node_plan import DGX1RelayNodePlan
 from sccl.ncclize import ncclize
-import re, subprocess, fcntl, tempfile, os, json, glob
+import re, subprocess, tempfile, os, json, atexit, time
 
-def init(num_subprocesses=1, logging=True):
-    if num_subprocesses > 1:
-        with open(os.path.join(tempfile.gettempdir(), 'sccl_autosynth_env.lock'), "a+") as f:
-            fcntl.lockf(f, fcntl.LOCK_EX)
-            try:
-                size = f.tell()
-                if size > 0:
-                    f.seek(0)
-                    env = json.load(f)
-                else:
-                    env = _autosynth_and_get_env(num_subprocesses, logging)
-                    json.dump(env, f)
-            finally:
-                fcntl.lockf(f, fcntl.LOCK_UN)
+def init(logging=True):
+    if 'LOCAL_WORLD_SIZE' in os.environ:
+        has_subprocesses = True
+        world_size = os.environ['WORLD_SIZE']
+        is_mpi_process = os.environ['LOCAL_RANK'] == 0
+        if logging:
+            print(f'SCCL: Found LOCAL_WORLD_SIZE in environment, torch.distributed.run detected, with {os.environ["LOCAL_WORLD_SIZE"]} subprocesses per machine.')
     else:
-        env = _autosynth_and_get_env(num_subprocesses, logging)
+        import argparse
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--local_rank", type=int)
+        args = parser.parse_known_args()
+        if args.local_rank != None:
+            has_subprocesses = True
+            world_size = os.environ['WORLD_SIZE']
+            is_mpi_process = args.local_rank == True
+            if logging:
+                print('SCCL: Found --local_rank N argument, legacy torch.distributed.launch detected, assuming one subprocess per GPU.')
+        else:
+            has_subprocesses = False
+            world_size = None
+            is_mpi_process = True
+            if logging:
+                print(f'SCCL: No launcher detected, assuming one MPI rank per process.')
+    # Name environment file by parent PID, which will be shared between subprocesses for torch.distributed.(launch|run)
+    env_file = os.path.join(tempfile.gettempdir(), f'sccl_autosynth_env.{os.getppid()}.lock')
+    if is_mpi_process:
+        env = _autosynth_and_get_env(world_size, logging)
+        if has_subprocesses:
+            # Make sure the lock file doesn't exist yet
+            if os.path.exists(env_file):
+                raise RuntimeError(f'SCCL: Lock file already exists: {env_file}')
+            # Broadcast algorithm to other subprocesses
+            with open(env_file, "w") as f:
+                json.dump(env, f)
+            # Delete the environment file at local rank 0 exit
+            atexit.register(os.remove(), env_file)
+    else:
+        assert has_subprocesses
+        # Wait until the environment file is available
+        elapsed = 0
+        while not os.path.exists(env_file):
+            time.sleep(1)
+            elapsed += 1
+            if elapsed == 10 and logging:
+                print(f'SCCL: Still waiting to read lock file {env_file}...')
+        # Load the environment to set from the file
+        with open(env_file, "r") as f:
+            env = json.load(f)
 
     os.environ.update(env)
 
-def _autosynth_and_get_env(num_subprocesses, logging):
+    if logging:
+        print('SCCL: Algorithms installed.')
+
+def _autosynth_and_get_env(world_size, logging):
     try:
         from mpi4py import MPI
     except ImportError as e:
         print('Please install the mpi4py package to use SCCL autosynth.')
         raise e
     comm = MPI.COMM_WORLD
-    size = comm.Get_size() * num_subprocesses
-    rank = comm.Get_rank()
+    mpi_size = comm.Get_size()
+    mpi_rank = comm.Get_rank()
+
+    if world_size == None:
+        world_size = mpi_size
 
     collective_names = ['Alltoall']
 
     machine = detect_machine(logging)
     plan = select_synthesis_plan(machine)
     names = comm.gather(machine[0], root=0)
-    if rank == 0:
+    if mpi_rank == 0:
         for i in range(len(names) - 1):
             if names[i] != names[i+1]:
                 raise RuntimeError(f'Rank {i} detected machine as {names[i]} but rank {i+1} detected machine as {names[i+1]}.')
         efs = []
         for name in collective_names:
-            algo = plan.synthesize(size, name, logging)
+            algo = plan.synthesize(world_size, name, logging)
             efs.append(ncclize(algo, old_format=True, use_scratch=True))
     else:
         efs = None
