a5ad03c38898ddf32ed00ed41c7726f63fdde1e5
Saeed Maleki <saemal@microsoft.com>
2021-07-23 02:54:26 +0000

    made a launcher for ndv2

---
+++ sccl/autosynth/ndv2_launcher.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+python -c "import sccl; sccl.ndv2_perm()"
+order=/var/lock/sccl_autosynth_inspector_topo.lock
+if [ -f "$order" ]; then
+    echo "Setting CUDA_VISIBLE_DEVICES to: "$CUDA_VISIBLE_DEVICES
+    export CUDA_VISIBLE_DEVICES=$(</var/lock/sccl_autosynth_inspector_topo.lock)
+fi
+$@
