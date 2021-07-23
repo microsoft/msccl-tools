68b1a635c32a5724311629c0848d1ec3216d0198
Saeed Maleki <saemal@microsoft.com>
2021-07-23 06:17:51 +0000

    silly bug

--- sccl/autosynth/ndv2_launcher.sh
+++ sccl/autosynth/ndv2_launcher.sh
@@ -3,6 +3,6 @@ python -c "import sccl; sccl.ndv2_perm()"
 order=/var/lock/sccl_autosynth_inspector_topo.lock
 if [ -f "$order" ]; then
     export CUDA_VISIBLE_DEVICES=$(</var/lock/sccl_autosynth_inspector_topo.lock)
-    echo "Setting CUDA_VISIBLE_DEVICES to: "$CUDA_VISIBLE_DEVICES
+    echo "Set CUDA_VISIBLE_DEVICES to: "$CUDA_VISIBLE_DEVICES
 fi
 $@
