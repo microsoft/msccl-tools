40feb7619d4a2381577b248ffbf9a4129b5b2bc9
Olli Saarikivi <olsaarik@microsoft.com>
2021-08-20 12:27:22 -0700

    Bump version to 2.2.0 due to API changes

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sccl',
-    version='2.1.2',
+    version='2.2.0',
     packages=find_packages(),
     entry_points={
         'console_scripts': [
