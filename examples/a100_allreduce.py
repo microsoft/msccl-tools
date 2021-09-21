f16c54c54e8572c5266be3b61ccaddec22e1f9e4
Meghan Cowan <meghancowan@microsoft.com>
2021-09-20 22:44:46 -0700

    parallel ways to allreduce

--- examples/a100_allreduce.py
+++ examples/a100_allreduce.py
@@ -7,19 +7,17 @@ from sccl.language import *
 from sccl.topologies import *
 from sccl.language.collectives import AllReduce
 
-tb_per_channel = 10
-def allreduce(instances):
+
+def allreduce(ways, instances):
     topology = fully_connected(8)
     size = topology.num_nodes() #  Number of gpus
-    logical_chunk = 8
+    logical_chunk = 8 * ways
+    tb_per_channel = 12
     collective = AllReduce(size, instances*logical_chunk, True, "allreduce")
     with SCCLProgram("allreduce_a100", topology, collective, instances*logical_chunk):
 
-        # 1 reduction between pairs number of gpus 
-        # Number of chunks reduced is depdendent on number of pairs
-        # 1 pair => 4 chunks, 2 pairs => 2 chunks
-        def reduce_ring(pairs, next_index, i, sendtb, recvtb):
-            count = 8 // (2*pairs)
+        # 1 reduction between pairs of gpus of count
+        def reduce_ring(pairs, count, next_index, lc, i, sendtb, recvtb):
             current_index = next_index.copy()
             for r in range(size):
                 next = r ^ pairs
@@ -30,40 +28,63 @@ def allreduce(instances):
                 block = 2 ** pairs
                 reverse = (r // block) % block
                 for x in range(count):
-                    if reverse == 0:
-                        x = count - 1 - x
-                    index = current_index[r] + offset + i * logical_chunk + x
+                    # if reverse == 0:
+                    #     x = count - 1 - x
+                    index = current_index[r] + offset + i*logical_chunk + lc*8 + x
                     c = Rank(r).input(index)
-                    c.reduce(next, Buffer.input, index, ch=i, sendtb=sendtb+i*tb_per_channel, recvtb=recvtb+i*tb_per_channel)
+                    c.reduce(next, Buffer.input, index, ch=lc + i * ways, sendtb=sendtb+i*tb_per_channel, recvtb=recvtb+i*tb_per_channel)
                 
 
         # Propagates reduced chunks in reverse order 
-        def propagate_ring(pairs, next_index, i, sendtb, recvtb):
-            count = 8 // (2*pairs)
+        def propagate_ring(pairs, count, next_index, lc, i, sendtb, recvtb):
             current_index = next_index.copy()            
             for r in range(size):
                 next = r ^ pairs
                 offset = (count if r > next else 0) 
                 next_index[r] -= offset
-                index = current_index[r] + i*logical_chunk
+                index = current_index[r] + i*logical_chunk + lc*8
                 c = Rank(r).input(index, count)
-                c.send(next, Buffer.input, index, ch=i, sendtb=sendtb+i*tb_per_channel, recvtb=recvtb+i*tb_per_channel)
+                c.send(next, Buffer.input, index, ch=lc + i * ways, sendtb=sendtb+i*tb_per_channel, recvtb=recvtb+i*tb_per_channel)
 
         for i in range(instances):
             next_index = [0] * 8
+            reduce_ring(1, 4, next_index, 0, i, 0, 1)
+            reduce_ring(2, 2, next_index, 0, i, 1, 2)
+            reduce_ring(4, 1, next_index, 0, i, 2, 3)
 
-            reduce_ring(1, next_index, i, 0, 1)
-            reduce_ring(2, next_index, i, 1, 2)
-            reduce_ring(4, next_index, i, 2, 3)
+            propagate_ring(4, 1, next_index, 0, i, 2, 3)
+            propagate_ring(2, 2, next_index, 0, i, 1, 2)
+            propagate_ring(1, 4, next_index, 0, i, 0, 1)
 
-            propagate_ring(4, next_index, i, 2, 3)
-            propagate_ring(2, next_index, i, 1, 2)
-            propagate_ring(1, next_index, i, 0, 1)
+            if ways > 1:
+                next_index = [0] * 8
+                lc = 1
+                reduce_ring(2, 4, next_index, lc, i, 4, 5)
+                reduce_ring(4, 2, next_index, lc, i, 5, 6)
+                reduce_ring(1, 1, next_index, lc, i, 6, 7)
+
+                
+                propagate_ring(1, 1, next_index, lc, i, 6, 7)
+                propagate_ring(4, 2, next_index, lc, i, 5, 6)
+                propagate_ring(2, 4, next_index, lc, i, 4, 5)
+
+            if ways > 2:
+                next_index = [0] * 8
+                lc = 2
+                reduce_ring(4, 4, next_index, lc, i, 8, 9)
+                reduce_ring(1, 2, next_index, lc, i, 9, 10)
+                reduce_ring(2, 1, next_index, lc, i, 10, 11)
+
+                propagate_ring(2, 1, next_index, lc, i, 10, 11)
+                propagate_ring(1, 2, next_index, lc, i, 9, 10)
+                propagate_ring(4, 4, next_index, lc, i, 8, 9)
 
         XML()
         Check()
 
 parser = argparse.ArgumentParser()
+parser.add_argument('ways', type=int, help='number of parallel ways to perform reduction min:1 max:3')
 parser.add_argument('instances', type=int, help='number of instances')
 args = parser.parse_args()
-allreduce(args.instances)
+assert args.ways >=0 and args.ways <= 3
+allreduce(args.ways, args.instances)
