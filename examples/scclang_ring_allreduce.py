8c076ab7d6be54eb148c82488f1a3cde8b8bd23d
Meghan Cowan <meghancowan@microsoft.com>
2021-09-22 15:40:23 -0700

    different ring

--- examples/scclang_ring_allreduce.py
+++ examples/scclang_ring_allreduce.py
@@ -6,7 +6,7 @@ from sccl.language import *
 from sccl.topologies import *
 from sccl.language.collectives import AllReduce
 
-def allreduce(instances):
+def allreduce_parallelrings(instances):
     size = 8
     topology = fully_connected(size)
     collective = AllReduce(size, size * instances, True, "allreduce")
@@ -28,8 +28,45 @@ def allreduce(instances):
         XML()
         Check()
 
+def allreduce_standard(instances):
+    size = 8
+    topology = fully_connected(size)
+    collective = AllReduce(size, size * instances, True, "allreduce")
+    with SCCLProgram("allreduce_ring", topology, collective, size * instances):
+        
+        
+        for i in range(instances):
+            # Get the chunk at rank r, input[r]
+            for step in range(0, size-1):
+                for chunk in range(0, size):
+                    index = chunk * instances + i
+                    start = (chunk + step) % size
+                    c = Rank(start).input(index)
+                    next = (chunk + step + 1) % size
+                    channel = (chunk%2) * instances + i
+                    c = c.reduce(next, Buffer.input, index, ch=channel, recvtb=channel, sendtb=channel)
+
+            for step in range(-1, size-2):
+                for chunk in range(0, size):
+                    index = chunk * instances + i
+                    start = (chunk + step) % size
+                    c = Rank(start).input(index)
+                    next = (chunk + step + 1) % size
+                    channel = (chunk%2) * instances + i
+                    c = c.send(next, Buffer.input, index, ch=channel, recvtb=channel, sendtb=channel)
+               
+               
+        XML()
+        Check()
+
+
 
 parser = argparse.ArgumentParser()
+parser.add_argument('version', type=int, help='which ring to run')
 parser.add_argument('instances', type=int, help='number of instances')
+
 args = parser.parse_args()
-allreduce(args.instances)
+if args.version == 0:
+    allreduce_standard(args.instances)
+else:
+    allreduce_parallelrings(args.instances)
