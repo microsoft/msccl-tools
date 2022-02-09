a07c242847d369a165fe4616118db8ea8675b9a1
Olli Saarikivi <olsaarik@microsoft.com>
2022-02-09 15:03:11 -0800

    Add additional registration for 64 NDV4 nodes

--- sccl/autosynth/ndv4_plans.py
+++ sccl/autosynth/ndv4_plans.py
@@ -21,3 +21,7 @@ def register_ndv4_plans():
     @register_sccl_program(fully_connected(8), 'alltoall', 'ndv4', protocol='Simple', sizes=('32MB', None), machines=lambda x: x == 8 or x == 16 or x == 32)
     def ndv4_alltoall(prog, nodes):
         alltoall_hierarchical(num_nodes=nodes, gpus_per_node=8)
+
+    @register_sccl_program(fully_connected(8), 'alltoall', 'ndv4', protocol='LL128', sizes=('1MB', '128MB'), machines=lambda x: x == 64)
+    def ndv4_alltoall(prog, nodes):
+        alltoall_hierarchical(num_nodes=nodes, gpus_per_node=8)
