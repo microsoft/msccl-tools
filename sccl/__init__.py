7aa9f794fb93e408ed33c091464f7bcf918206d5
Olli Saarikivi <olsaarik@microsoft.com>
2022-02-15 18:27:06 -0800

    Actually import new plans printing to sccl.*

--- sccl/__init__.py
+++ sccl/__init__.py
@@ -1,6 +1,6 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 
-from sccl.autosynth import init
+from sccl.autosynth import init, tabulate_plans, print_plans
 from sccl.autosynth import ndv2_perm
 from sccl.autosynth import Collective
