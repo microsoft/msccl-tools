a08ca8ce35672268adbf5eec687177c8380e1b2d
Meghan Cowan <meghancowan@microsoft.com>
2022-04-19 09:45:03 -0700

    In place reduce gather

--- examples/scclang/reducegather.py
+++ examples/scclang/reducegather.py
@@ -18,14 +18,26 @@ class ReduceGather(Collective):
         assert self.chunk_factor == 1
         rank_buffers = []
         chunks_per_node = self.num_ranks
-        for r in range(self.num_ranks):
-            input_buffer = [None] * self.gpus_per_group
-            output_buffer = [None] * chunks_per_node
-            for c in range(self.groups):
-                input_buffer[c] = Chunk(r, c, -1, c)
-            buffers = {Buffer.input : input_buffer, 
-                       Buffer.output : output_buffer}
-            rank_buffers.append(buffers)
+        if self.inplace:
+            # Only an output buffer for inplace
+            for r in range(self.num_ranks):
+                output_buffer = [None] * chunks_per_node
+                group = r // self.gpus_per_group
+                for c in range(self.gpus_per_group):
+                    output_buffer[group*self.gpus_per_group + c] = Chunk(r, c, -1, self.gpus_per_group*group+c)
+                input_buffer = output_buffer[group*self.gpus_per_group:(group+1)*self.gpus_per_group]
+                buffers = {Buffer.input: input_buffer,
+                    Buffer.output : output_buffer}
+                rank_buffers.append(buffers)
+        else:
+            for r in range(self.num_ranks):
+                input_buffer = [None] * self.gpus_per_group
+                output_buffer = [None] * chunks_per_node
+                for c in range(self.gpus_per_group):
+                    input_buffer[c] = Chunk(r, c, -1, c)
+                buffers = {Buffer.input : input_buffer, 
+                        Buffer.output : output_buffer}
+                rank_buffers.append(buffers)
         return rank_buffers
             
 
@@ -33,7 +45,7 @@ class ReduceGather(Collective):
         expected_chunks = []
         for r in range(self.num_ranks):
             chunk = ReduceChunk([])
-            for x in range(self.groups):
+            for x in range(self.gpus_per_group):
                 y = r // self.groups
                 next = y * self.groups + x
                 chunk = chunk.reduce(Chunk(next, r % self.gpus_per_group))
@@ -49,15 +61,56 @@ class ReduceGather(Collective):
                     correct = False
         return correct
 
+    # For inplace Reduce Gather the input buffer is pointer into the output buffer
+    def get_buffer_index(self, rank, buffer, index):
+        if self.inplace and buffer == Buffer.input:
+            group = rank // self.gpus_per_group
+            return Buffer.output, index + group * self.gpus_per_group
+        else:
+            return buffer, index
 
-def program(num_ranks, groups, instances, protocol):
+
+# Inplace reduce gather
+def reduce_gather_ip(num_ranks, groups, instances, protocol):
+    gpus_per_group = num_ranks // groups
+    topology = fully_connected(num_ranks)
+    chunk_factor = 1
+    inplace = True
+    collective = ReduceGather(num_ranks, chunk_factor, inplace, groups)
+
+    with SCCLProgram("reduce-gather", topology, collective, instances, protocol, threadblock_policy=ThreadblockPolicy.auto):
+
+        # Per group reduce scatter
+        for y in range(groups):
+            for x in range(gpus_per_group):
+                output_index = y * groups + x
+                input_index = x
+                gpu = y * groups + (x+1) % gpus_per_group
+                c = chunk(gpu, Buffer.output, output_index)
+                # Use the input buffer to perform reduction across groups
+                for x_ in range(1, gpus_per_group):
+                    c = c.reduce(y * groups + (x + 1 + x_) % gpus_per_group, Buffer.input, input_index)
+
+        # Ring Allgather
+        for r in range(num_ranks):
+            c = chunk(r, Buffer.output, r)
+            next = (r + 1) % num_ranks
+            while next != r:
+                c = c.send(next, Buffer.output, r)
+                next = (next + 1) % num_ranks
+
+        Check()
+        XML()
+
+# Out of place reduce gather 
+def reduce_gather_oop(num_ranks, groups, instances, protocol):
     gpus_per_group = num_ranks // groups
     topology = fully_connected(num_ranks)
     chunk_factor = 1
     inplace = False
     collective = ReduceGather(num_ranks, chunk_factor, inplace, groups)
 
-    with SCCLProgram("reduce-gather", topology, collective, instances, protocol, threadblock_policy=ThreadblockPolicy.manual):
+    with SCCLProgram("reduce-gather", topology, collective, instances, protocol, threadblock_policy=ThreadblockPolicy.auto):
 
         # Per group reduce scatter
         for y in range(groups):
@@ -68,9 +121,9 @@ def program(num_ranks, groups, instances, protocol):
                 c = chunk(gpu, Buffer.input, input_index)
                 # Use the input buffer to perform reduction across groups
                 for x_ in range(1, gpus_per_group):
-                    c = c.reduce(y * groups + (x + 1 + x_) % gpus_per_group, Buffer.input, input_index, sendtb=0, recvtb=0, ch=0)
+                    c = c.reduce(y * groups + (x + 1 + x_) % gpus_per_group, Buffer.input, input_index)
                 # Copy reduced chunk into the output buffer
-                c = c.send(c.rank, Buffer.output, output_index, sendtb=0, recvtb=0, ch=0)
+                c = c.send(c.rank, Buffer.output, output_index)
 
 
         # Ring Allgather
@@ -78,7 +131,7 @@ def program(num_ranks, groups, instances, protocol):
             c = chunk(r, Buffer.output, r)
             next = (r + 1) % num_ranks
             while next != r:
-                c = c.send(next, Buffer.output, r, sendtb=1, recvtb=1, ch=1)
+                c = c.send(next, Buffer.output, r)
                 next = (next + 1) % num_ranks
 
         Check()
@@ -91,8 +144,12 @@ if __name__ == '__main__':
     parser.add_argument('--instances', type=int, default=1, help='number of instances')
     parser.add_argument('--protocol', type=str, default='Simple', 
         choices=['Simple', 'LL', 'LL128'], help ='NCCL protocol')
+    parser.add_argument('--outofplace', action='store_true')
     args = parser.parse_args()
 
     assert args.num_ranks % args.groups == 0
 
-    program(args.num_ranks, args.groups, args.instances, args.protocol)
+    if args.outofplace:
+        reduce_gather_oop(args.num_ranks, args.groups, args.instances, args.protocol)
+    else:
+        reduce_gather_ip(args.num_ranks, args.groups, args.instances, args.protocol)
