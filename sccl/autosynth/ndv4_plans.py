ce1cd1159b1a4a26708c6fb466a1f8a22bd8c98d
Olli Saarikivi <olsaarik@microsoft.com>
2022-04-22 14:50:55 -0700

    Update readme and cleanup algorithm registrations

--- sccl/autosynth/ndv4_plans.py
+++ sccl/autosynth/ndv4_plans.py
@@ -15,18 +15,14 @@ def register_ndv4_plans():
     def ndv4_ring_allreduce(prog, nodes):
         allreduce_ring(size=8, channels=8)
 
-    @register_sccl_program(fully_connected(8), 'alltoall', 'ndv4', protocol='LL128', sizes=('1MB', '32MB'), machines=lambda x: x == 8 or x == 16 or x == 32)
-    def ndv4_alltoall(prog, nodes):
+    @register_sccl_program(fully_connected(8), 'alltoall', 'ndv4', protocol='LL128', sizes=('1MB', '32MB'), machines=lambda x: x == 8 or x == 16 or x == 32 or x == 64)
+    def ndv4_alltoall_hierarchical(prog, nodes):
         alltoall_hierarchical(num_nodes=nodes, gpus_per_node=8)
 
     @register_sccl_program(fully_connected(8), 'alltoall', 'ndv4', protocol='Simple', sizes=('32MB', None), machines=lambda x: x == 8 or x == 16 or x == 32)
-    def ndv4_alltoall(prog, nodes):
-        alltoall_hierarchical(num_nodes=nodes, gpus_per_node=8)
-
-    @register_sccl_program(fully_connected(8), 'alltoall', 'ndv4', protocol='LL128', sizes=('1MB', '32MB'), machines=lambda x: x == 64)
-    def ndv4_alltoall(prog, nodes):
+    def ndv4_alltoall_hierarchical(prog, nodes):
         alltoall_hierarchical(num_nodes=nodes, gpus_per_node=8)
 
     @register_sccl_program(fully_connected(8), 'alltoall', 'ndv4', protocol='Simple', sizes=('32MB', None), machines=lambda x: x == 64)
-    def ndv4_alltoall(prog, nodes):
+    def ndv4_alltoall_three_step(prog, nodes):
         alltoall_three_step(num_nodes=nodes, gpus_per_node=8)
