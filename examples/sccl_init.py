94eb7cfbb4c8acdc3ba2bd62e37d0ce8030b03ea
Olli Saarikivi <olsaarik@microsoft.com>
2022-05-10 13:44:35 -0700

    Fix sccl init example

--- examples/sccl_init.py
+++ examples/sccl_init.py
@@ -4,14 +4,13 @@
 import os
 
 def show():
-    print()
-    print(f"SCCL_CONFIG = {os.environ['SCCL_CONFIG']}")
-    print(f"NCCL_MIN_NCHANNELS = {os.environ['NCCL_MIN_NCHANNELS']}")
-    print(f"NCCL_NET_SHARED_BUFFERS = {os.environ['NCCL_NET_SHARED_BUFFERS']}")
-    print(f"Contents of {os.environ['SCCL_CONFIG']}:")
-    with open(os.environ['SCCL_CONFIG']) as f:
-        print(f.read())
-    print()
+    if 'SCCL_CONFIG' in os.environ:
+        print()
+        print(f"SCCL_CONFIG = {os.environ['SCCL_CONFIG']}")
+        print(f"Contents of {os.environ['SCCL_CONFIG']}:")
+        with open(os.environ['SCCL_CONFIG']) as f:
+            print(f.read())
+        print()
 
 
 print('=== Trigger a builtin synthesis plan ===')
@@ -57,32 +56,33 @@ sccl.init('ndv9000', 1, (sccl.Collective.alltoall, ('2KB', None)))
 show()
 
 
-print('=== SCCLang program ===')
+# TODO: Update the following programs to use the new syntax
+# print('=== SCCLang program ===')
 
-from sccl.autosynth.registry import register_sccl_program
-from sccl.topologies import line
-from sccl.language import *
+# from sccl.autosynth.registry import register_sccl_program
+# from sccl.topologies import line
+# from sccl.language import *
 
-@register_sccl_program(line(2), 'allgather', 'two_gpus', machines= lambda m: m == 1)
-def trivial_allgather(prog, nodes):
-    chunk(Buffer.input, 0, 0).send(0, Buffer.output, 0).send(1)
-    chunk(Buffer.input, 1, 0).send(1, Buffer.output, 1).send(0)
+# @register_sccl_program(line(2), 'allgather', 'two_gpus', machines= lambda m: m == 1)
+# def trivial_allgather(prog, nodes):
+#     chunk(Buffer.input, 0, 0).send(0, Buffer.output, 0).send(1)
+#     chunk(Buffer.input, 1, 0).send(1, Buffer.output, 1).send(0)
 
-sccl.init('two_gpus', 1, (sccl.Collective.allgather, (0, None)))
+# sccl.init('two_gpus', 1, (sccl.Collective.allgather, (0, None)))
 
-show()
+# show()
 
 
-print('=== SCCLang program example ====')
+# print('=== SCCLang program example ====')
 
-from sccl.topologies import fully_connected
-from sccl.programs.allreduce_a100_ring import allreduce_ring
+# from sccl.topologies import fully_connected
+# from sccl.programs.allreduce_a100_ring import allreduce_ring
 
-@register_sccl_program(fully_connected(8), 'allreduce', 'ndv4', chunk_factor=8, inplace=True,
-    instances=4, protocol='LL128', threadblock_policy=ThreadblockPolicy.manual, machines=lambda x: x == 1)
-def ndv4_ring_allreduce(prog, nodes):
-    allreduce_ring(size=8, channels=8)
+# @register_sccl_program(fully_connected(8), 'allreduce', 'ndv4', chunk_factor=8, inplace=True,
+#     instances=4, protocol='LL128', threadblock_policy=ThreadblockPolicy.manual, machines=lambda x: x == 1)
+# def ndv4_ring_allreduce(prog, nodes):
+#     allreduce_ring(size=8, channels=8)
 
-sccl.init('ndv4', 1, (sccl.Collective.allreduce, (0, None)))
+# sccl.init('ndv4', 1, (sccl.Collective.allreduce, (0, None)))
 
-show()
+# show()
