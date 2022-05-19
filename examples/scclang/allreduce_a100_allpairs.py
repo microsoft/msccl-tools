da41eb5f3ac39e206b4eebb5c84953ecb852c4db
Meghan Cowan <meghancowan@microsoft.com>
2022-05-19 14:50:53 -0700

    making programs parameterized

--- examples/scclang/allreduce_a100_allpairs.py
+++ examples/scclang/allreduce_a100_allpairs.py
@@ -6,9 +6,9 @@ from sccl.language import *
 from sccl.topologies import *
 from sccl.language.collectives import AllReduce
 
-def allreduce_allpairs(instances, protocol):
-    size = 8
-    chunksperloop = 64
+def allreduce_allpairs(gpus, instances, protocol):
+    size = gpus
+    chunksperloop = gpus * gpus
     topology = fully_connected(size)
     collective = AllReduce(size, chunksperloop, True)
     with SCCLProgram("allreduce_pairs", topology, collective, instances, protocol=protocol, 
@@ -18,32 +18,33 @@ def allreduce_allpairs(instances, protocol):
         for r1 in range(size):
             for r2 in range(size):
                 if r1 != r2:
-                    index = r2 * 8
-                    c = chunk(r1, Buffer.input, index, size=8)
-                    c.copy(r2, 'scratch', sendtb=r2, recvtb=r1, ch=0)
+                    index = r2 * size
+                    c = chunk(r1, Buffer.input, index, size=size)
+                    c.copy(r2, 'scratch', sendtb=r2, recvtb=r1)
 
         # Each rank performs a local reduction on the nth chunk
         # Utilize 8 threadblocks for this reduction for better parallelism
         for r in range(size):
-            for index in range(0, 56):
-                    c = chunk(r, Buffer.input, r*8 + (index % 8))
-                    c.reduce(chunk(r, 'scratch', index), sendtb=(index % 8), ch=0)
+            for index in range(0, size * (size-1)):
+                    c = chunk(r, Buffer.input, r*size + (index % size))
+                    c.reduce(chunk(r, 'scratch', index), sendtb=(index % size))
         
         # Each rank sends the fully reduced nth chunk to all other gpus
         for r1 in range(size):
             for r2 in range(size):
                 if r1 != r2:
-                    index = r1 * 8
-                    c = chunk(r1, Buffer.input, index, 8)
+                    index = r1 * size
+                    c = chunk(r1, Buffer.input, index, size)
                     c.copy(r2, Buffer.input, index, sendtb=r2, recvtb=r1)
                 
         XML()
         Check()
 
 parser = argparse.ArgumentParser()
+parser.add_argument('num_gpus', type=int, help ='number of gpus')
 parser.add_argument('instances', type=int, help='number of instances')
 parser.add_argument('--protocol', type=str, default='LL', choices=['Simple', 'LL128', 'LL'], help='Protocol')
 
 args = parser.parse_args()
 
-allreduce_allpairs(args.instances, args.protocol)
+allreduce_allpairs(args.num_gpus, args.instances, args.protocol)
--- examples/scclang/alltoall_allpairs.py
+++ examples/scclang/alltoall_allpairs.py
@@ -22,7 +22,7 @@ def alltoall(num_ranks, instances, protocol):
 parser = argparse.ArgumentParser()
 parser.add_argument('num_gpus', type=int, help ='number of gpus')
 parser.add_argument('instances', type=int, help ='number of instances')
-parser.add_argument('protocol', type=str, default='Simple', choices=['Simple', 'LL', 'LL128'], help ='NCCL protocol. Default: Simple')
+parser.add_argument('--protocol', type=str, default='Simple', choices=['Simple', 'LL', 'LL128'], help ='NCCL protocol. Default: Simple')
 args = parser.parse_args()
 
 alltoall(args.num_gpus, args.instances, args.protocol)
