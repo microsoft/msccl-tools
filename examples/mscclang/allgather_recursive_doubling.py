251dd26f9ae0ee40c8e2b0241c9f0e154b6516af
Meghan Cowan <meghancowan@microsoft.com>
2022-09-26 17:20:21 -0700

    Fix - no reference to chunks that doesn't exist (#42)
    * Fix AllGather recursive doubling
    
    * No reference to non-existent chunk

--- examples/mscclang/allgather_recursive_doubling.py
+++ examples/mscclang/allgather_recursive_doubling.py
@@ -9,16 +9,15 @@ from msccl.language.collectives import AllGather
 # https://web.cels.anl.gov/~thakur/papers/mpi-coll.pdf
 def allgather_recursive_doubling(size, instances, protocol):
     topology = fully_connected(size)
-    collective = AllGather(size, instances, True)
-    with MSCCLProgram("allgather_recursive_doubling", topology, collective, 1, protocol=protocol, threadblock_policy=ThreadblockPolicy.manual):
+    collective = AllGather(size, 1, True)
+    with MSCCLProgram("allgather_recursive_doubling", topology, collective, instances, protocol=protocol, threadblock_policy=ThreadblockPolicy.manual):
         count = 1
         while count < size:
             # Every rank exchanges count chunks with neighbor count away
             for rank in range(size):
-                for i in range(instances):
-                    peer = rank ^ count
-                    index = ((rank // count) * count) * instances + i * count
-                    chunk(rank, Buffer.output, index, size=count).copy(peer, Buffer.output, index, sendtb=peer, recvtb=rank) 
+                peer = rank ^ count
+                index = (rank // count) * count
+                chunk(rank, Buffer.output, index, size=count).copy(peer, Buffer.output, index, sendtb=peer, recvtb=rank) 
             count *= 2
 
         XML()
